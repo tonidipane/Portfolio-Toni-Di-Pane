b367540648ea2d7290d4a476fe3279e319c2c207
Toni Di Pane <toni.dipane.contact@gmail.com>
2026-01-18 21:20:09 +0100

    Add files via upload

--- program.js
+++ program.js
@@ -120,4 +120,51 @@ function animate() {
 animate();
 
 
+const cardDiv = document.getElementById('cardDiv');
+const imgCard = document.getElementById('card2');
+
+
+
+cardDiv.addEventListener('mouseenter', (e) => {
+    bounds = cardDiv.getBoundingClientRect();
+    rotateToMouse(e)
+    document.addEventListener('mousemove', rotateToMouse);
+});
   
+cardDiv.addEventListener('mouseleave', () => {
+    document.removeEventListener('mousemove', rotateToMouse);
+    cardDiv.style.transform = '';
+    cardDiv.style.background = '';
+    imgCard.style.filter = `
+        drop-shadow(5px 5px 20px rgba(168, 168, 168, 0))
+    `;
+});
+
+
+
+function rotateToMouse(e) {
+    const mouseX = e.clientX;
+    const mouseY = e.clientY;
+    const leftX = mouseX - bounds.x;
+    const topY = mouseY - bounds.y;
+    const center = {
+      x: leftX - bounds.width / 2,
+      y: topY - bounds.height / 2
+    }
+    const distance = Math.sqrt(center.x**2 + center.y**2);
+
+    cardDiv.style.transform = `
+        perspective(60em)
+        scale3d(1.07, 1.07, 1.07)
+        rotate3d(
+            ${center.y / 100},
+            ${-center.x / 100},
+            0,
+            ${Math.log(distance)* 2}deg
+        )
+    `
+
+    imgCard.style.filter = `
+        drop-shadow(5px 5px 15px rgba(168, 168, 168, 0.62))
+    `;
+  }
